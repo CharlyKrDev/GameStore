a8e95b348e216f435c4007a6a184e2d5105579d6
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2023-12-22 05:52:36 +0100

    formulario de contacto

---
+++ js/form.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var contactForm = document.getElementById('contactForm');
+
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        if (validarFormulario()) {
+            contactForm.submit();
+        }
+    });
+
+
+function validarFormulario() {
+    const nameInput = document.getElementById('name');
+    const phoneInput = document.getElementById('phone');
+    const messageInput = document.getElementById('message');
+
+    if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]+$/.test(nameInput.value)) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Por favor, ingrese solo letras en el campo de nombre.",
+        });
+        return false;
+    }
+
+    if (!/^\d+$/.test(phoneInput.value)) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Por favor, ingrese solo números en el campo de número de contacto.",
+        });
+        return false;
+    }
+    if (/^\s*$/.test(messageInput.value)){
+
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "El mensaje no puede estar vacio",
+        });
+        return false;
+    }
+
+    return true;
+}
+});
--- js/index.js
+++ js/index.js
@@ -3,83 +3,56 @@ import { agregarAlCarrito } from "./carrito.js";
 const contenedorCatalogo = document.getElementById('contenedorJuegos')
 
 export function crearCatalogoJuegos(productos) {
-
-    productos.forEach(producto => {
-
+    productos.map(producto => {
         const { img, nombre, descripcion, precioAnterior, precioVigente } = producto;
 
         const nuevosJuegos = document.createElement('article');
-
         nuevosJuegos.classList.add('cardsCatalogoJuegos');
 
         const imgElement = document.createElement('img');
-
         imgElement.src = img;
 
         const h2Element = document.createElement('h2');
-
         h2Element.textContent = nombre;
 
         const pElement = document.createElement('p');
-
         pElement.textContent = descripcion;
 
         const preciosSection = document.createElement('section');
-
         preciosSection.classList.add('precios');
 
         const precioAnteriorSpan = document.createElement('span');
-
         precioAnteriorSpan.classList.add('precioAnterior');
-
         precioAnteriorSpan.textContent = `u$s ${precioAnterior}`;
 
         const precioVigenteText = document.createTextNode(` u$s ${precioVigente}`);
-
         preciosSection.appendChild(precioAnteriorSpan);
-
         preciosSection.appendChild(precioVigenteText);
 
         const buttonElement = document.createElement('button');
-
         buttonElement.textContent = 'Comprar';
 
         nuevosJuegos.appendChild(imgElement);
-
         nuevosJuegos.appendChild(h2Element);
-
         nuevosJuegos.appendChild(pElement);
-
         nuevosJuegos.appendChild(preciosSection);
-
         nuevosJuegos.appendChild(buttonElement);
 
-        contenedorCatalogo.appendChild(nuevosJuegos);
-
         buttonElement.addEventListener('click', () => {
-
             agregarAlCarrito(producto);
 
             Toastify({
-
                 text: `¡${nombre} añadido al carrito!`,
-
                 duration: 1000,
-
                 close: true,
-
                 gravity: "top",
-
                 style: {
-
                     background: "linear-gradient(to right, rgba(36, 36, 36, 0.4), rgba(138, 43, 226, 0.6))"
-
                 },
-
             }).showToast();
 
         });
 
+        contenedorCatalogo.appendChild(nuevosJuegos);
     });
-
 }
